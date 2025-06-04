e832a7877fc49ec1d87ff14b56b5ecc839003685
justkiduss <kidusteshome036@gmail.com>
2025-06-04 08:30:10 +0300

    fixing error in build 2

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: 'http://localhost:5173',"https://whispr-net-f.vercel.app",
+		origin: ['http://localhost:5173', 'https://whispr-net-f.vercel.app'],
 		credentials: true,
 	})
 );
